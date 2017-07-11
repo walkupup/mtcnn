7f3ad1c36287c0700ee56829ff6b047e883048b3
hopef <512690069@qq.com>
2017-07-11 10:25:19 +0800

    修改批处理中文乱码问题

--- train/make-lmdb-12.bat
+++ train/make-lmdb-12.bat
@@ -1,21 +1,9 @@
 @echo off
 
-set tips_info=0
-if exist train_lmdb12 set tips_info=1
-
-if %tips_info% == 1 (
-    echo 确定要创建吗，这个操作会删掉已有目录的？，防止误操作把好不容易创建的删除掉啦
-    pause
-)
-
-
 if exist train_lmdb12 rd /q /s train_lmdb12
 
-rem 如果要设计图像尺寸的，比如，可以加参数--resize_width=224 --resize_height=224
-rem 如果要讲输入图像弄成单通道灰度图，可以加参数--gray=true
-
-echo 创建训练数据库train_lmdb12...
+echo create train_lmdb12...
 "caffe/convert_imageset.exe" "" 12/label-train.txt train_lmdb12 --backend=mtcnn --shuffle=true
 
-echo LMDB数据库创建完毕。
+echo done.
 pause
